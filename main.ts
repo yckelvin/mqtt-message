96a7db81b49c8404c9746c28edd5897e63e4e248
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-07-13 00:23:44 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
+    if (message == "smile") {
+        basic.showIcon(IconNames.Happy)
+    } else if (message == "sad") {
+        basic.showIcon(IconNames.Sad)
+    } else if (message == "yes") {
+        basic.showIcon(IconNames.Yes)
+    } else if (message == "no") {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # # .
+            . . . . .
+            . . # . .
+            `)
+    }
+})
+let wifi_name = "izowifi"
+let password = "izo1234@"
+let iot_id = "lmZB9bXGR"
+let iot_pwd = "liWfrxXMgz"
+let topic_0 = "qwPmNL37g"
+microIoT.microIoT_initDisplay()
+microIoT.microIoT_WIFI(wifi_name, password)
+microIoT.microIoT_MQTT(
+iot_id,
+iot_pwd,
+topic_0,
+microIoT.SERVERS.English
+)
