45f998f9b40a316725fc9c5ea19ae4c624cbe80b
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:08:08 +0800

    add debug

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
         basic.showIcon(IconNames.Yes)
     } else if (message == "no") {
         basic.showIcon(IconNames.No)
+        pins.analogWritePin(AnalogPin.P1, 1023)
     } else {
         basic.showLeds(`
             . # # # .
@@ -17,16 +18,20 @@ microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
             `)
     }
 })
+basic.showNumber(0)
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(1)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(2)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(3)
